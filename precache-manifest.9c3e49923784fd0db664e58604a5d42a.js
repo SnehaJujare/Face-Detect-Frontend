ef623dc1d9cff01789109aeb9ca44c3572e2a452
SnehaJujare <jujaresneha@gmail.com>
2020-05-18 21:05:31 +0530

    Updates

---
+++ precache-manifest.9c3e49923784fd0db664e58604a5d42a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc21955125afab0c075aa761bc02ea45",
+    "url": "/Face-Detect-Frontend/index.html"
+  },
+  {
+    "revision": "3ca976c5a663197855e9",
+    "url": "/Face-Detect-Frontend/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a12c9537f09412762ff6",
+    "url": "/Face-Detect-Frontend/static/css/main.f5fcd1fe.chunk.css"
+  },
+  {
+    "revision": "3ca976c5a663197855e9",
+    "url": "/Face-Detect-Frontend/static/js/2.3289414c.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/Face-Detect-Frontend/static/js/2.3289414c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a12c9537f09412762ff6",
+    "url": "/Face-Detect-Frontend/static/js/main.4b360d62.chunk.js"
+  },
+  {
+    "revision": "43ab2063149ab622c23a",
+    "url": "/Face-Detect-Frontend/static/js/runtime-main.2b45aa19.js"
+  }
+]);
